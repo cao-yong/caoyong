02e0a76cc95f6eb139f396b001c4a290b8ff50a1
yong.cao <yong.cao@kxtx.cn>
2017-07-05 00:27:31 +0800

    fix table structure

--- cy-common/src/main/resources/table.sql
+++ cy-common/src/main/resources/table.sql
@@ -1,20 +1,3 @@
-/*
-SQLyog 企业版 - MySQL GUI v8.14 
-MySQL - 5.1.49-community : Database - caoyong
-*********************************************************************
-*/
-
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`caoyong` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
 USE `caoyong`;
 
 /*Table structure for table `cy_brand` */
@@ -32,7 +15,7 @@ CREATE TABLE `cy_brand` (
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_time` datetime NOT NULL COMMENT '修改时间',
   `creator` varchar(10) NOT NULL DEFAULT 'SYSTEM' COMMENT '创建者',
-  `modifier` varbinary(10) NOT NULL DEFAULT 'SYSTEM' COMMENT '修改者',
+  `modifier` varchar(10) NOT NULL DEFAULT 'SYSTEM' COMMENT '修改者',
   `extra_info` varchar(128) DEFAULT NULL COMMENT '扩展字段',
   `is_deleted` char(1) NOT NULL DEFAULT 'N' COMMENT '是否删除（Y：是，N：否）',
   PRIMARY KEY (`id`,`is_deleted`)
@@ -40,7 +23,7 @@ CREATE TABLE `cy_brand` (
 
 /*Data for the table `cy_brand` */
 
-insert  into `cy_brand`(`id`,`name`,`description`,`img_url`,`web_site`,`sort`,`is_display`,`create_time`,`update_time`,`creator`,`modifier`,`extra_info`,`is_deleted`) values (1,'依琦莲9','212','http://192.168.128.128/group1/M00/00/00/wKiAgFlKid2AFectAAFaL_xdW_s975.jpg',NULL,99,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(2,'凯速（KANSOON）','红色好看的','http://192.168.128.128/group1/M00/00/00/wKiAgFlKj4KAL4LjAAGVD2i3LAI402.jpg',NULL,98,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(3,'梵歌纳（vangona）','蓝色经典','http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,97,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(4,'伊朵莲',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,96,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(5,'菩媞',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlKj4KAL4LjAAGVD2i3LAI402.jpg',NULL,95,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(6,'丹璐斯',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlKj4KAL4LjAAGVD2i3LAI402.jpg',NULL,94,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(7,'喜悦瑜伽',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,93,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(8,'皮尔（pieryoga）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,92,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(9,'路伊梵（LEFAN）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,91,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(10,'金啦啦',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,90,0,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(11,'来尔瑜伽（LaiErYoGA）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,89,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(12,'艾米达（aimida）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,88,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(13,'斯泊恩',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,87,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(14,'康愫雅KSUA',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,86,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(15,'格宁',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,85,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(16,'E奈尔（Enaier）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,84,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(17,'哈他',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,83,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(18,'伽美斯（Jamars）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,82,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(19,'瑜伽树（yogatree）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,81,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(20,'兰博伊人（lanboyiren）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,80,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y');
+insert  into `cy_brand`(`id`,`name`,`description`,`img_url`,`web_site`,`sort`,`is_display`,`create_time`,`update_time`,`creator`,`modifier`,`extra_info`,`is_deleted`) values (1,'依琦莲9','212','http://192.168.128.128/group1/M00/00/00/wKiAgFlKid2AFectAAFaL_xdW_s975.jpg',NULL,99,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(2,'凯速（KANSOON）','红色好看的','http://192.168.128.128/group1/M00/00/00/wKiAgFlKj4KAL4LjAAGVD2i3LAI402.jpg',NULL,98,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'Y'),(3,'梵歌纳（vangona）','蓝色经典','http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,97,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(4,'伊朵莲',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,96,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(5,'菩媞',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlKj4KAL4LjAAGVD2i3LAI402.jpg',NULL,95,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(6,'丹璐斯',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlKj4KAL4LjAAGVD2i3LAI402.jpg',NULL,94,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(7,'喜悦瑜伽',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,93,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(8,'皮尔（pieryoga）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,92,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(9,'路伊梵（LEFAN）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,91,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(10,'金啦啦',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,90,0,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(11,'来尔瑜伽（LaiErYoGA）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,89,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(12,'艾米达（aimida）','','http://192.168.128.128/group1/M00/00/00/wKiAgFlU_ZCANFVoAABkep5Lr6M450.jpg',NULL,88,1,'2017-06-25 16:37:08','2017-06-29 21:15:47','SYSTEM','SYSTEM',NULL,'N'),(13,'斯泊恩',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,87,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(14,'康愫雅KSUA',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,86,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(15,'格宁',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,85,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(16,'E奈尔（Enaier）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,84,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(17,'哈他',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,83,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(18,'伽美斯（Jamars）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,82,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(19,'瑜伽树（yogatree）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,81,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N'),(20,'兰博伊人（lanboyiren）',NULL,'http://192.168.128.128/group1/M00/00/00/wKiAgFlNI8eACYbCAAGaUioeBjg175.jpg',NULL,80,1,'2017-06-25 16:37:08','2017-06-25 16:37:08','SYSTEM','SYSTEM',NULL,'N');
 
 /*Table structure for table `cy_buyer` */
 
@@ -81,7 +64,7 @@ CREATE TABLE `cy_color` (
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_time` datetime NOT NULL COMMENT '修改时间',
   `creator` varchar(10) NOT NULL DEFAULT 'SYSTEM' COMMENT '创建者',
-  `modifier` varbinary(10) NOT NULL DEFAULT 'SYSTEM' COMMENT '修改者',
+  `modifier` varchar(10) NOT NULL DEFAULT 'SYSTEM' COMMENT '修改者',
   `extra_info` varchar(128) DEFAULT NULL COMMENT '扩展字段',
   `is_deleted` char(1) NOT NULL DEFAULT 'N' COMMENT '是否删除（Y：是，N：否）',
   PRIMARY KEY (`id`)
@@ -149,7 +132,7 @@ CREATE TABLE `cy_order` (
 DROP TABLE IF EXISTS `cy_product`;
 
 CREATE TABLE `cy_product` (
-  `id` bigint(20) NOT NULL COMMENT 'ID或商品编号',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID或商品编号',
   `brand_id` bigint(20) DEFAULT NULL COMMENT '品牌ID',
   `name` varchar(64) DEFAULT NULL COMMENT '商品名称',
   `weight` varchar(64) DEFAULT NULL COMMENT '重量 单位:克',
@@ -169,18 +152,18 @@ CREATE TABLE `cy_product` (
   `extra_info` varchar(128) DEFAULT NULL COMMENT '扩展字段',
   `is_deleted` char(1) DEFAULT 'N' COMMENT '是否删除（Y：是，N：否）',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品';
+) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=utf8 COMMENT='商品';
 
 /*Data for the table `cy_product` */
 
-insert  into `cy_product`(`id`,`brand_id`,`name`,`weight`,`is_new`,`is_hot`,`is_commend`,`is_show`,`img_url`,`description`,`package_list`,`colors`,`sizes`,`creator`,`modifier`,`create_time`,`update_time`,`extra_info`,`is_deleted`) values (1,11111,'test','1',1,1,1,1,'www.baidu.com','test','test','test','test','SYSTEM','SYSTEM',NULL,NULL,NULL,'N'),(2,22222,'testing','2',1,1,1,1,'www.jd.com','testing','testing','testing','testing','SYSTEM','SYSTEM',NULL,NULL,NULL,'N');
+insert  into `cy_product`(`id`,`brand_id`,`name`,`weight`,`is_new`,`is_hot`,`is_commend`,`is_show`,`img_url`,`description`,`package_list`,`colors`,`sizes`,`creator`,`modifier`,`create_time`,`update_time`,`extra_info`,`is_deleted`) values (1,11111,'test','1',1,1,1,1,'/images/t/t3/e2.jpg','test','test','test','test','SYSTEM','SYSTEM',NULL,NULL,NULL,'N'),(2,22222,'testing','2',1,1,1,1,'/images/t/t3/e1.jpg','testing','testing','testing','testing','SYSTEM','SYSTEM',NULL,NULL,NULL,'N'),(3,3,'潮流假期瑜伽服健身服女士三件套四件套跑步运动套装女春秋宽松健身房速干上衣套装 粉红色-短袖四件套 M','0.6',1,1,1,1,'http://192.168.128.128/group1/M00/00/00/wKiAgFlVIveAAnajAAAIUYMv6ZM393.jpg,http://192.168.128.128/group1/M00/00/00/wKiAgFlVIveADCljAAAIm8yzLwM555.jpg,http://192.168.128.128/group1/M00/00/00/wKiAgFlVIveAOUHmAAAHnbsfCZk605.jpg,http://192.168.128.128/group1/M00/00/00/wKiAgFlVIveABKKJAAAItM1cFQ8733.jpg,http://192.168.128.128/group1/M00/00/00/wKiAgFlVIveAFRSWAAAInaZAoSk925.jpg','<img src=\"http://192.168.128.128/group1/M00/00/00/wKiAgFlVIwuAb6LsAAQZThCyCH8136.jpg\" alt=\"\" />','衣服*1，裤子*1','9,10,12,14,16,23,25','S,M,L,XL,XXL','SYSTEM','SYSTEM','2017-06-29 23:55:59','2017-06-29 23:55:59',NULL,'N'),(1001,5,'测试数据生成商品id','0.6',NULL,NULL,1,0,'http://192.168.128.128/group1/M00/00/00/wKiAgFlWhr6AKi1SAAFaL_xdW_s782.jpg','<img src=\"http://192.168.128.128/group1/M00/00/00/wKiAgFlWhs-AR6_2AAD-yBA5sAQ409.jpg\" alt=\"\" />','衣服*2，裤子*1','9,11,12',NULL,'SYSTEM','SYSTEM','2017-07-01 01:14:08','2017-07-01 01:14:08',NULL,'N');
 
 /*Table structure for table `cy_sku` */
 
 DROP TABLE IF EXISTS `cy_sku`;
 
 CREATE TABLE `cy_sku` (
-  `id` bigint(20) NOT NULL COMMENT 'ID',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `product_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
   `color_id` bigint(20) DEFAULT NULL COMMENT '颜色ID',
   `size` varchar(16) DEFAULT NULL COMMENT '尺码',
@@ -196,11 +179,24 @@ CREATE TABLE `cy_sku` (
   `extra_info` varchar(128) DEFAULT NULL COMMENT '扩展字段',
   `is_deleted` char(1) DEFAULT 'N' COMMENT '是否删除（Y：是，N：否）',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='最小销售单元';
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COMMENT='最小销售单元';
 
 /*Data for the table `cy_sku` */
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+insert  into `cy_sku`(`id`,`product_id`,`color_id`,`size`,`market_price`,`price`,`delive_fee`,`stock`,`upper_limit`,`creator`,`modifier`,`create_time`,`update_time`,`extra_info`,`is_deleted`) values (1,3,9,'S','89.50','129.00','10.00',221,20076,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(2,3,9,'M','0.00','43.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(3,3,9,'L','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(4,3,9,'XL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(5,3,9,'XXL','0.00','3.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(6,3,10,'S','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(7,3,10,'M','0.00','3.20','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(8,3,10,'L','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(9,3,10,'XL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(10,3,10,'XXL','0.00','343','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(11,3,12,'S','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(12,3,12,'M','0.00','34','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(13,3,12,'L','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(14,3,12,'XL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(15,3,12,'XXL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(16,3,14,'S','0.00','34','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(17,3,14,'M','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(18,3,14,'L','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(19,3,14,'XL','0.00','10','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(20,3,14,'XXL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(21,3,16,'S','0.00','7.23','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(22,3,16,'M','0.00','67','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(23,3,16,'L','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(24,3,16,'XL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(25,3,16,'XXL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(26,3,23,'S','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(27,3,23,'M','0.00','32.23','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(28,3,23,'L','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(29,3,23,'XL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(30,3,23,'XXL','0.00','0.99','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(31,3,25,'S','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(32,3,25,'M','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(33,3,25,'L','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(34,3,25,'XL','0.00','0.00','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N'),(35,3,25,'XXL','0.00','32.32','10.00',0,200,'SYSTEM','SYSTEM','2017-06-29 23:56:00','2017-06-29 23:56:00',NULL,'N');
+
+/*Table structure for table `test_tb` */
+
+DROP TABLE IF EXISTS `test_tb`;
+
+CREATE TABLE `test_tb` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(24) DEFAULT NULL,
+  `birthday` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
+
+/*Data for the table `test_tb` */
+
+insert  into `test_tb`(`id`,`name`,`birthday`) values (1,'刘一手','2017-06-01 00:23:44'),(2,'张一国','2017-06-01 00:29:39'),(3,'卡星球','2017-06-01 21:41:38'),(5,'范冰冰','2017-06-01 21:59:28'),(6,'范冰冰','2017-06-01 22:02:30'),(34,'李冰冰','2017-06-05 23:08:40'),(35,'李冰冰','2017-06-05 23:08:40'),(36,'李冰冰','2017-06-05 23:15:01');
+
