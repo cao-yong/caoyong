13dcb19bb5fd081f954deb4048aca6cda45c3df3
caoyong <caoyong@lvmama.com>
2017-12-29 18:53:03 +0800

    change portal system to spring boot.

---
+++ cy-portal/src/main/java/com/caoyong/Application.java
@@ -0,0 +1,57 @@
+package com.caoyong;
+
+import com.caoyong.common.conversion.CustomConverter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 入口
+ *
+ * @author caoyong 2017年10月24日 下午3:29:57
+ */
+@EnableScheduling
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return (container) -> {
+            ErrorPage error403Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/static/403.html");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/static/404.html");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/static/500.html");
+            container.addErrorPages(error403Page, error404Page, error500Page);
+        };
+    }
+
+    @Bean(name = "conversionService")
+    public ConversionServiceFactoryBean getConversionService() {
+        ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
+        Set<Converter> converters = new HashSet<>();
+        converters.add(new CustomConverter());
+        bean.setConverters(converters);
+        return bean;
+    }
+
+}
